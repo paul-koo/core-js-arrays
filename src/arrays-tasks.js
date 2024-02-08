7e5401600dfd0295e97fbfd24ba1f6d11778b69d
paul-koo <pavelkoltun999@gmail.com>
2024-02-09 00:27:46 +0300

    init: start core-js-arrays task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let arr = [];
+    for (let i=start; start<=end; i++) {
+        arr.push(i);
+    }
+    return arr;
 }
 
 /**
